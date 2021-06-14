e36832897550308ba46d4368571952ec3be6b47a
Mateusz Choma <kontakt@coderoad.pl>
2021-06-14 19:16:58 +0200

    Rendering list of tasks

--- src/ToDo.js
+++ src/ToDo.js
@@ -4,10 +4,12 @@ export class ToDo extends React.Component {
     state = {
       tasks: [
         {
+          id: '123',
           text: 'Wynieś śmieci',
           isCompleted: false
         },
         {
+          id: '321',
           text: 'Zmyj naczynia',
           isCompleted: false
         }
@@ -15,9 +17,21 @@ export class ToDo extends React.Component {
     }
 
     render () {
+      const { tasks } = this.state
+
       return (
         <div>
-          ToDo
+          <ul>
+            {
+              tasks.map(({ id, text }) => {
+                return (
+                  <li key={id}>
+                    {text}
+                  </li>
+                )
+              })
+            }
+          </ul>
         </div>
       )
     }
