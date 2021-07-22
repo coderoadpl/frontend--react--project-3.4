c30e6c0206ed21cd2c81481ae123c2fd32d10355
Mateusz Choma <kontakt@coderoad.pl>
2021-07-22 09:56:19 +0200

    `Button` styled

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
 import classes from './styles.module.css'
 
 export const Button = (props) => {
   const {
+    children,
     className,
     ...otherProps
   } = props
@@ -13,11 +17,18 @@ export const Button = (props) => {
     <button
       className={`${classes.root}${className ? ` ${className}` : ''}`}
       {...otherProps}
-    />
+    >
+      {children}
+      {' '}
+      <FontAwesomeIcon
+        icon={faTrash}
+      />
+    </button>
   )
 }
 
 Button.propTypes = {
+  children: PropTypes.node,
   className: PropTypes.string
 }
 
